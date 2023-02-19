bc88b064ff316ac0a60f4bdd29b158269176fd66
Saman Batool <samanbatool08@gmail.com>
2023-02-19 16:52:27 -0500

    signup form component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Morning from './Components/Morning';
 import NavBar from './Components/NavBar';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Welcome />
       <Morning />
     </div>
   );
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -5,13 +5,26 @@ const Welcome = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
 
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+
   return (
     <div>
       <h1>Welcome User</h1>
 
       <form>
         <label>Username: </label>
-        <input name="username" value={username} type="text" 
+        <input name="username" value={username} type="text" onChange={handleUsernameChange}></input>
+
+        <label>Email: </label>
+        <input name="email" value={email} type="text" onChange={handleEmailChange}></input>
+
+        <input type="submit" value="Submit"></input>
       </form>
     </div>
   )
